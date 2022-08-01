9cd03c37c7e5c378720d36cd70dd855abeee61c4
ruzny <roony0782045336@gmail.com>
2022-08-01 12:51:24 +0530

    root-query setup

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
-require("dotenv").config()
+const dotenv = require("dotenv")
+const { graphqlHTTP } = require("express-graphql")
+const schema = require("./schema/schema")
+
+dotenv.config();
 
 const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development'
+}))
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`listening on port ${process.env.PORT || 3000}`);
